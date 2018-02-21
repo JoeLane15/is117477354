1b0c6594e305ccbc7cc63315ef9c2fee9ffe9f23
Joseph <117477354@umail.ucc.ie>
2018-02-21 14:48:18 +0000

    Salesforce

--- eBusiness/Ebus3.php
+++ eBusiness/Ebus3.php
@@ -1,17 +1,17 @@
 <?php
 session_start();
-?> 
-<!DOCTYPE html> 
+?>
+<!DOCTYPE html>
 <html>
     <head>
-        <tile>Receipt</tile>
+        <title>RECEIPT</title>
     </head>
     <body>
-        <h4>Receipt</h4>
-        
+        <h4>RECEIPT</h4>
+                
         <?php
-        // Echo session variables that were set on previous page 
-        echo "Total is " .  $session["total"] . ".";
+        // Echo session variables that were set on previous page
+        echo "Total is " . $_SESSION["total"] . ".";
         ?>
     </body>
 </html>
--- eBusiness/cost_calc.js
+++ eBusiness/cost_calc.js
@@ -2,31 +2,31 @@
 
 function calcSub(){
     
-    var argSubTotal; 
+    var argSubTotal;
     
-    if(document.getElementById('salesforce').checked){
-    argSubTotal=100;
-}
-    else{ 
-        argSubTotal=300;
+    if(document.getElementById('salesforce').checked) {
+      argSubTotal = 100;
+    }
+    else {
+      argSubTotal = 300;
     }
     
     display(argSubTotal);
 }
 
 function display(parm1){
-    
-    document.getElementById("subtotal").value= parm1;
-    document.getElementById("total").value= parm1;
-    
-    enablebtnProceed();
-    
-    
-}   
+  
+  document.getElementById("subtotal").value = parm1;
+  document.getElementById("total").value = parm1;
+        
+  enablebtnProceed();
+}
+
+
 function enablebtnProceed(){
-    $('#btnProceed').prop('disabled',false);
-    
-} 
-function enablebtnProceed(){
-    $('#btnProceed').prop('disabled',true); 
+    $('#btnProceed').prop('disabled', false);
+}
+
+function disablebtnProceed() {
+    $('#btnProceed').prop('disabled', true);
 }
