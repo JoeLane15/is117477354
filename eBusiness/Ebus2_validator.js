f2ce2dcd53a22dfca133d08716ee93fb1a7b9f18
Joseph <117477354@umail.ucc.ie>
2018-02-22 09:59:29 +0000

    Ebus2 and cloud services

--- eBusiness/Ebus2_validator.js
+++ eBusiness/Ebus2_validator.js
@@ -22,6 +22,6 @@ function enablebtnPurchase(){
     $('#btnPurchase').prop('disabled', false);
 }
 
-function disablebtnPurchase() {
+function disablebtnPurchase(){
     $('#btnPurchase').prop('disabled', true);
 }
